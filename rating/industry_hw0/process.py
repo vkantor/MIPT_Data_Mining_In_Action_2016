6d006051053f88451640b4b6713a865ccfa9b267
Ilya Irhin <tylorn@tylorn.dev.ydf.yandex.net>
2016-10-30 14:24:24 +0300

    update table

--- rating/industry_hw0/process.py
+++ rating/industry_hw0/process.py
@@ -3,8 +3,9 @@ import json
 import chardet
 
 df = pandas.read_csv('industry_hw0', sep='\t')
+df.mae = map(lambda x: float(x.replace(',', '.')), df.mae)
 values = sorted(df.mae)
-bound_best = 0
+bound_best = 0.0
 bound_fst = values[-50]
 bound_snd = values[-100]
 
